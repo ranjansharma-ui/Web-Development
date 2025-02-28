9c662fc5a680e9381b7227afbeac94eb21d9bbc1
ranjan <ranjankumar4278907@gmail.com>
2025-02-28 10:35:31 +0530

    JavaScript

---
+++ JavaScript/Variables/index.js
@@ -0,0 +1,66 @@
+// var age = 25;
+
+
+// if(true){
+//     console.log(age)
+// }
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+    
+// }
+
+// console.log(age);
+// solve()
+
+
+// var x = 10;
+// var x = 20;
+
+// let -> Block-scope
+
+// {
+// let a = 10;
+// console.log(a);
+// }
+// console.log(a);
+
+//not possible
+// let a = 20;
+// let a = 30;
+
+// // posible
+// let a= 23;
+// a = 10;
+
+
+// let x = 10;
+// x = "Ranjan";
+
+// const a = 20;
+// console.log(a);
+
+// a = 20 // not possible
+
+
+// let marks = 20;
+// marks = "Babber";
+// marks = true;
+
+// let marks;
+// console.log(marks);
+// let value = null;
+// console.log(value);
+
+let marks = 9840833957983279572979859287985729875927987982989759872987598;
+
+console.log(marks);
+console.log(typeof(marks))
+
+
+
+
+
+
+
---
+++ JavaScript/Variables/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var age = 25;
+
+
+if(true){
+    console.log(age)
+}
