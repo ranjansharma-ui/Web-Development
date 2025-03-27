98cdbd20862bf3dbdaad96688a55997585f3627f
ranjan <ranjankumar4278907@gmail.com>
2025-03-27 15:31:12 +0530

    react

---
+++ React_js/Hooks/useRef-hook/All-exapmles/ex-3/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <nav>
+      <button onClick={() => scrollToIndex(0)}>
+        cat</button>
+        
+    </nav>
+   </div>
+  )
+}
+
+export default App
